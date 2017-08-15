30818076ac20a7f2c23ae8342dfbd72e15b1f49a
William E Bodell III <ratzbodell@gmail.com>
2017-08-15 12:37:46 -0400

    ERC20 and Contracts extension support - Modify newSpecGenerator.py to generate spec pages from three rdf files - Work in progress, still needs testing

--- newSpecGenerator.py
+++ newSpecGenerator.py
@@ -1,14 +1,27 @@
 import ontospy
 from ontodocs.viz.viz_html_multi import *
 
-RDFpath = "https://raw.githubusercontent.com/ConsenSys/EthOn/master/EthOn.rdf"
-outputPath = "spec"
+class SpecGenerator():
+    '''
+    Generates specification from an ontology
+    '''
 
-# load current ontology from EthOn github
-g = ontospy.Ontospy(RDFpath)
+    def __init__(self, rdf_path, output_path):
+        RDFpath = rdf_path
+        outputPath = "spec"
 
-# invoke EthOn multi-HTML page visualizer
-v = KompleteViz(g, "EthOn: Ethereum Ontology", "paper")
+        # load current ontology from EthOn github
+        g = ontospy.Ontospy(RDFpath)
 
-# build HTML
-v.build(outputPath)
+        # invoke EthOn multi-HTML page visualizer
+        v = KompleteViz(g, "EthOn: Ethereum Ontology", "paper")
+
+        # build HTML
+        v.build(outputPath)
+
+def main():
+    SpecGenerator(rdf_path="https://raw.githubusercontent.com/ConsenSys/EthOn/master/EthOn.rdf", output_path="spec")
+    SpecGenerator(rdf_path="https://raw.githubusercontent.com/ConsenSys/EthOn/master/ERC20/EthOn_ERC20.rdf", output_path="spec/ERC20")
+    SpecGenerator(rdf_path="https://raw.githubusercontent.com/ConsenSys/EthOn/master/Contracts/EthOn_Contracts.rdf", output_path="spec/Contracts")
+
+main()
